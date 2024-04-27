7527e7a90823a4f60f4ed27c556783e0866418dc
Anton Sokolskyi <sokolplus3783@gmail.com>
2024-04-27 14:38:20 +0300

    update constructor

--- src/main/java/com/example/clearsolutions/model/DateFilter.java
+++ src/main/java/com/example/clearsolutions/model/DateFilter.java
@@ -1,9 +1,13 @@
 package com.example.clearsolutions.model;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DateFilter {
 
   @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date should format in 'yyyy-mm-dd'")
